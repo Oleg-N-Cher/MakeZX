3aaa8f34fffef874353362cdcdcab0686ded345e
Oleg N. Cher <allot@bk.ru>
2012-12-10 16:18:54 +0200

    Update scripts '+Compile.bat' and 'Obj/+Clear.bat'.

--- +Compile.bat
+++ +Compile.bat
@@ -4,16 +4,25 @@
 @CD Obj
 @DEL *.o
 
-@IF EXIST %obj%\Console.h COPY /Y /B %obj%\Console.h
-@IF EXIST %obj%\Console.c COPY /Y /B %obj%\Console.c
-@IF EXIST %obj%\CmdLine.h COPY /Y /B %obj%\CmdLine.h
-@IF EXIST %obj%\CmdLine.c COPY /Y /B %obj%\CmdLine.c
-@IF EXIST %obj%\MakeZX.c COPY /Y /B %obj%\MakeZX.c
+@IF EXIST %obj%\Platform.h MOVE /Y %obj%\Platform.h
+@IF EXIST %obj%\Platform.c MOVE /Y %obj%\Platform.c
+@IF EXIST %obj%\Console.h MOVE /Y %obj%\Console.h
+@IF EXIST %obj%\Console.c MOVE /Y %obj%\Console.c
+@IF EXIST %obj%\CmdLine.h MOVE /Y %obj%\CmdLine.h
+@IF EXIST %obj%\CmdLine.c MOVE /Y %obj%\CmdLine.c
+@IF EXIST %obj%\Files.h MOVE /Y %obj%\Files.h
+@IF EXIST %obj%\Files.c MOVE /Y %obj%\Files.c
+@IF EXIST %obj%\TapeTAP.h MOVE /Y %obj%\TapeTAP.h
+@IF EXIST %obj%\TapeTAP.c MOVE /Y %obj%\TapeTAP.c
+@IF EXIST %obj%\MakeZX.c MOVE /Y %obj%\MakeZX.c
 
 @IF NOT EXIST SYSTEM.o %tcc% -c SYSTEM.c
+@IF NOT EXIST Platform.o %tcc% -c Platform.c
 @IF NOT EXIST Console.o %tcc% -c Console.c
 @IF NOT EXIST CmdLine.o %tcc% -c CmdLine.c
-%tcc% MakeZX.c SYSTEM.o Console.o CmdLine.o
+@IF NOT EXIST Files.o %tcc% -c Files.c
+@IF NOT EXIST TapeTAP.o %tcc% -c TapeTAP.c
+%tcc% MakeZX.c SYSTEM.o Platform.o Console.o CmdLine.o Files.o TapeTAP.o -omakezx.exe
 
 @IF EXIST MakeZX.exe MOVE MakeZX.exe ..
 @PAUSE
--- Obj/+Clear.bat
+++ Obj/+Clear.bat
@@ -1,3 +1,6 @@
 @DEL *.o
+@DEL *.obj
+@DEL *.tds
+@DEL *.~*
 @ATTRIB -H *.sbm
 @DEL *.sbm
