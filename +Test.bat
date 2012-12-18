3c3a1f04a311ad38dd1e1f4cd0e7bf655187b6a6
Oleg N. Cher <allot@bk.ru>
2012-12-18 21:11:47 +0200

    Add script '+Test.bat'.

---
+++ +Test.bat
@@ -0,0 +1,38 @@
+makezx.exe par1 2TAP "3333A AAAAAAA" 1.Tap 555555555555555555.tAP
+@PAUSE
+makezx.exe .Tap
+@PAUSE
+makezx.exe ta\pe.bin
+@PAUSE
+makezx.exe tape.bin:FFFF
+@PAUSE
+makezx.exe tape.bin:FFFFhh
+@PAUSE
+makezx.exe tape.bin:0FFFFH
+@PAUSE
+makezx.exe tape.bin:1AG tape.bin:2AG
+@PAUSE
+makezx.exe :3AG
+@PAUSE
+makezx.exe :2
+@PAUSE
+makezx.exe a:
+@PAUSE
+makezx.exe :H
+@PAUSE
+makezx.exe :FHH
+@PAUSE
+makezx.exe tape.tap tape.bin
+@PAUSE
+makezx.exe tape.bin tape.tap
+@PAUSE
+makezx.exe \bin.bin
+@PAUSE
+makezx.exe C:\bin.bin
+@PAUSE
+makezx.exe ../bin.bin:100000
+@PAUSE
+makezx.exe ../bin.bin:4000H bom.bin:7D00H
+@PAUSE
+makezx.exe ../bin.bin:4000H bom.bin:26000 BOM.TaP
+@PAUSE
